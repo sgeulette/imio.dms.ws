d8ba48e2f558e7d3a17990f3d147e90aad923942
Stephan Geulette <s.geulette@imio.be>
2014-03-11 16:23:38 +0100

    Add new route to test usage of post json parameters

--- src/imio/dms/ws/__init__.py
+++ src/imio/dms/ws/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+import zope.component
 
 from zope.i18nmessageid import MessageFactory
+from z3c.json import interfaces
+from z3c.json import converter
 
 _ = MessageFactory('imio.dms.ws')
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+    zope.component.provideUtility(converter.JSONReader(), interfaces.IJSONReader)
--- src/imio/dms/ws/dmsfile.py
+++ src/imio/dms/ws/dmsfile.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from zope.component import getUtility
+from z3c.json import interfaces
 from plone.jsonapi.core.browser import router
+from AccessControl import getSecurityManager
+from AccessControl import Unauthorized
 
 
 @router.add_route("/test", "test", methods=["GET"])
 def test(context, request):
     return {
         "url": router.url_for("test", force_external=True),
+    }
+
+
+@router.add_route("/send_dmsfile", "send_dmsfile", methods=["POST"])
+def send_dmsfile(context, request):
+    """
+        Webservice method to send a dms file and create a plone object
+    """
+#    if not getSecurityManager().checkPermission("Manage portal", context):
+#        raise Unauthorized("You cannot access this webservice")
+
+    jsonReader = getUtility(interfaces.IJSONReader)
+    input_params = jsonReader.read(request.get('json', ''))
+    return {
+        'input': input_params
     }
