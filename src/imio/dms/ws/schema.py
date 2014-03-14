fc38d4545faeea3cf741c2e588dbc4df1cff41ab
Stephan Geulette <s.geulette@imio.be>
2014-03-14 13:23:13 +0100

    Added string constrain

--- src/imio/dms/ws/schema.py
+++ src/imio/dms/ws/schema.py
@@ -9,18 +9,22 @@ input_schema = {
         "barcode": {
             "description": "The unique identifier of a document",
             "type": "string",
+            "pattern": "^.+$",
         },
         "type": {
             "description": "The type of the document",
             "type": "string",
+            "minLength": 1,
         },
         "pages_number": {
             "description": "The number of pages",
             "type": "integer",
+            "minimum": 1,
         },
         "client_id": {
             "description": "The id of the client",
             "type": "string",
+            "minLength": 1,
         },
         "scan_date": {
             "description": "The scan date with format DDMMYYYY",
@@ -48,6 +52,7 @@ input_schema = {
             "description": "The file content encoded in base64. '+' and '/' characters have to be "
                            "literally replaced by '-' and '_' characters",
             "type": "string",
+            "minLength": 1,
         },
     },
     "additionalProperties": False,
